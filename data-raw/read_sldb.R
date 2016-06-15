a291c530dd9e88062ec5e6f1b30d702adabc27fd
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-15 18:11:56 -0400

    Revamp all data building and checking
    Thorough use of assertr to check that values are present and unique when they ought to be. Also, dt_compiled does all the joining and spreading work necessary to create the real training data for the model. Filtered subsets of this main table can then be more easily used in modeling code - with 80% of the data carpentry finished ahead of time.

--- data-raw/read_sldb.R
+++ data-raw/read_sldb.R
@@ -3,6 +3,7 @@ library(tidyr)
 library(stringr)
 library(googlesheets)
 import::from(plyr, revalue)
+library(assertr)
 
 sldb <- gs_title("sldb")
 
@@ -24,9 +25,17 @@ dt_painting_motifs <- dt_paintings %>%
   select(painting_code, significant_motifs) %>%
   separate(significant_motifs, into = paste0("motif_", 1:7), sep = "; ", extra = "drop", fill = "right") %>%
   gather(code_no, motif_code, contains("motif"), na.rm = TRUE) %>%
-  select(-code_no)
+  select(-code_no) %>%
+  distinct() %>%
+  assert(not_na, painting_code, motif_code)
 
-dt_paintings <- dt_paintings %>% select(-significant_motifs, -artist)
+# Create a predicate that returns false if a column has duplicated values
+is_unique <- function(x) anyDuplicated(x) == 0
+
+dt_paintings <- dt_paintings %>%
+  select(-significant_motifs, -artist) %>%
+  assert(not_na, painting_code, compositional_format, compositional_disposition, compositional_cropping, compositional_viewpoint, year_early, year_late, is_fragment, support, illusionistic_signature, has_inscribed_date) %>%
+  verify(is_unique(painting_code))
 
 # Unique table of motif codes and their labels
 dt_motif_labels <- raw_motifs %>%
@@ -34,26 +43,44 @@ dt_motif_labels <- raw_motifs %>%
   left_join(select(raw_motifs, motif_code, p2_label = motif_label), by = c("p2" = "motif_code")) %>%
   mutate(full_motif_label = ifelse(is.na(p1_label), motif_label, ifelse(is.na(p2_label), paste(p1_label, motif_label, sep = " - "), paste(p1_label, p2_label, motif_label, sep = " - ")))) %>%
   mutate(wrapped_motif_label = str_wrap(full_motif_label, width = 30)) %>%
-  select(motif_code, motif_label = full_motif_label, wrapped_motif_label)
+  select(motif_code, motif_label = full_motif_label, wrapped_motif_label) %>%
+  verify(is_unique(motif_code)) %>%
+  assert(not_na, motif_code, motif_label, wrapped_motif_label)
 
 # Many to many table of motif codes and their parent values
 dt_motif_taxonomy <- raw_motifs %>%
   select(-motif_label) %>%
   gather(parent_no, parent, p1:p2) %>%
   mutate(is_top_level = parent_no == "p1" & is.na(parent)) %>%
-  select(-parent_no)
+  select(-parent_no) %>%
+  assert(not_na, motif_code)
 
 # Create artists tables ----
 
 # Make an artist attributes table with life dates
-dt_painting_artist <- bind_rows(
-  select(raw_artists, painting_code, artist = artist1, artist_relationship = artist1_relationship),
-  select(raw_artists, painting_code, artist = artist2, artist_relationship = artist2_relationship),
-  select(raw_artists, painting_code, artist = artist3, artist_relationship = artist3_relationship)
-) %>%
-  filter(!is.na(artist) & !is.na(artist_relationship)) %>%
-  distinct()
+dt_painting_artist <- raw_artists %>%
+  select(-artist_string) %>%
+  filter(!is.na(artist1)) %>%
+  assert(not_na, painting_code, artist1, artist1_relationship) %>%
+  verify(is_unique(painting_code))
 
+# This is a compiled table that merges the dt_paintings_, dt_motif_, and
+# dt_artist_ tables above into one table with one row per painting, and all
+# available variables for that painting.
+dt_compiled <- dt_painting_motifs %>%
+  left_join(dt_motif_taxonomy, by = "motif_code") %>%
+  mutate(parent = ifelse(is.na(parent), motif_code, parent)) %>%
+  select(-motif_code, -is_top_level) %>%
+  mutate(is_present = TRUE) %>%
+  distinct() %>%
+  filter(!is.na(parent)) %>%
+  spread(key = parent, value = is_present, fill = FALSE, drop = FALSE) %>%
+  verify(is_unique(painting_code)) %>%
+  inner_join(select(dt_painting_artist, painting_code, artist = artist1, artist_relationship = artist1_relationship), by = "painting_code") %>%
+  verify(is_unique(painting_code)) %>%
+  # Attach compositional data
+  inner_join(dt_paintings, by = "painting_code") %>%
+  verify(is_unique(painting_code))
 
-devtools::use_data(dt_paintings, dt_painting_motifs, dt_motif_labels, dt_motif_taxonomy, dt_painting_artist, dt_artist_attributes, overwrite = TRUE)
-
+# Write all the tables to the package data directory
+devtools::use_data(dt_paintings, dt_painting_motifs, dt_motif_labels, dt_motif_taxonomy, dt_painting_artist, dt_artist_attributes, dt_compiled, overwrite = TRUE)
