8ee07f564572221e0581d1847b578dde33f8da18
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-29 16:59:30 -0400

    Build more PCA interpretation functions

--- R/model_interpretation.R
+++ R/model_interpretation.R
@@ -20,6 +20,37 @@ rf_votes <- function(rf) {
   add_rownames(as.data.frame(rf$votes), var = "painting_code")
 }
 
+#' Create a PCA of local importance measures from a random forest
+#' @export
+pca_rf <- function(rf, .filter = function(x) return(x)) {
+  rf_local_importance(rf) %>%
+    .filter %>%
+    stats::prcomp()
+}
+
+#' Local Importance as a data frame
+#' @export
+rf_local_importance <- function(rf) {
+  data.frame(t(rf$localImp)) %>%
+    add_rownames()
+}
+
+#' Calculate local variable loadings
+#' @export
+pca_loadings_df <- function(rf_pca, m = 200) {
+  data.frame(rf_pca$rotation)[,1:2] %>%
+    add_rownames() %>%
+    mutate(power = sqrt(PC1^2 + PC2^2)) %>%
+    filter(min_rank(desc(power)) <= m)
+}
+
+#' Return observation locations in PCA space
+#' @export
+pca_obs_df <- function(rf_pca, rownames = "rownames") {
+  data.frame(rf_pca$x)[,1:2] %>%
+    add_rownames(var = rownames)
+}
+
 # Internal functions ----
 
 confusion <- function(f) {
@@ -32,12 +63,3 @@ long_confusion <- function(rf) {
     add_rownames("actual") %>%
     tidyr::gather(projected, count, -actual, -class.error)
 }
-
-rf_local_importance <- function(rf) {
-  data.frame(t(rf$localImp))
-}
-
-pca_rf <- function(rf) {
-  stats::prcomp(rf_local_importance(rf))
-}
-
