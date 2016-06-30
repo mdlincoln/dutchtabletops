de706c53f88478685962ebdee7ebee653e75bf03
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-06-30 14:42:12 -0400

    plotting helper function

---
+++ R/plotting.R
@@ -0,0 +1,12 @@
+#' Render ggplot while also writing multiple versions to disk
+#'
+#' @param p Plot object
+#' @param filetypes Types of files to save
+#' @param filename Filenames
+#' @param ... Options to pass to \code{\link[ggplot2]{ggsave}}
+#'
+#' @export
+multiplot <- function(p, filetypes = c("pdf", "svg", "png"), filenames = paste0("inst/plots/", filetypes, "/", deparse(substitute(p)), ".", filetypes), ...) {
+  walk(filenames, function(x) ggplot2::ggsave(filename = x, plot = p, ...))
+  plot(p)
+}
