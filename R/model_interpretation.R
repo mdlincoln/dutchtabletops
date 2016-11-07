32460cad9c56d767efdfed3a7b1381131d8685e2
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2016-11-06 21:52:25 -0800

    implement a raggedy rf effects visualizer
    I've no idea why gather can't find the right column names unless I print() them - this defies everything I've come to expect with R.

--- R/model_interpretation.R
+++ R/model_interpretation.R
@@ -99,8 +99,44 @@ top_n_importance <- function(rf, class, n) {
 
 #' @describeIn top_n_importance Return the names as a vector only
 #' @export
-top_n_importance_names <- function(rf, class, n) {
+top_n_importance_names <- function(rf, class, n = 9) {
   top_n_importance(rf, class, n)[["term"]]
+}
+
+#' Plot effects of certain variables on the predicitons of a given random forest model
+#'
+#' @param rf A RandomForest object
+#' @param class Character. The predicted class name
+#' @param terms Character. Quoted term names. Defaults to the top 9 most important terms for the given variable as calculated by \link{top_n_importance_names}.
+#'
+#' @export
+rf_effects <- function(rf, class, terms = top_n_importance_names(rf = rf, class = class)) {
+
+  print(c("class: ", class))
+  print(c("terms: ", terms))
+
+  orig_data <- tibble::rownames_to_column(rf_x(rf), var = rf_rownames(rf)) %>%
+    mutate(actual = as.character(rf_y(rf)))
+
+  print(c("orig_data: ", names(orig_data)))
+
+  srf <- rf_votes(rf) %>%
+    gather_(key_col = "predicted", value_col = "votes", gather_cols = setdiff(names(.), rf_rownames(rf))) %>%
+    inner_join(orig_data, by = rf_rownames(rf)) %>%
+    filter(predicted == class) %>%
+    select(one_of(c("votes", "actual", terms)))
+
+  print(c("pre gather: ", names(srf)))
+
+  srf <- srf %>%
+    gather_(key_col = "term", value_col = "metric_value", gather_cols = setdiff(names(.), c("votes", "actual"))) %>%
+    mutate(
+      term = factor(term, levels = terms),
+      is_true_positive = actual == class)
+
+  print(c("final: ", names(srf)))
+
+  srf
 }
 
 # Internal functions ----
